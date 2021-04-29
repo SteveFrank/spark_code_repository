92a2038fb1c6fa833b1eeed8da697bd55f981b80
FrankQian <yangqian3@staff.weibo.com>
2021-04-29 14:52:46 +0800

    init : wc 项目初始化

---
+++ scala_code_repository/src/main/scala/com/spark/scala/lesson/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author yangqian
+ * @date 2021/2/22
+ */
+package com.spark.scala.lesson;
---
+++ wordcount/src/main/java/com/spark/study/App.java
@@ -0,0 +1,8 @@
+package com.spark.study;
+
+/**
+ * @author yangqian
+ * @date 2021/4/29
+ */
+public class App {
+}
