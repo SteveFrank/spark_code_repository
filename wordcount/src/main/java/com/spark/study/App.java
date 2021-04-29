5c987ba4431b549453171416ec073dc25eb084cc
FrankQian <yangqian3@staff.weibo.com>
2021-04-29 14:53:54 +0800

    init : Hello Spark world

--- wordcount/src/main/java/com/spark/study/App.java
+++ wordcount/src/main/java/com/spark/study/App.java
@@ -5,4 +5,9 @@ package com.spark.study;
  * @date 2021/4/29
  */
 public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World! Spark World! Nice To Meet You!");
+    }
+
 }
