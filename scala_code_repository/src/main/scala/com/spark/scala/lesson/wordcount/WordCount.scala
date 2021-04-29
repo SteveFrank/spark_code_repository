7daf59f87e56b5320009d40234ecc75fc5ea25ad
FrankQian <yangqian3@staff.weibo.com>
2021-04-29 17:12:43 +0800

    init : WordCount

---
+++ scala_code_repository/src/main/scala/com/spark/scala/lesson/wordcount/WordCount.scala
@@ -0,0 +1,23 @@
+package com.spark.scala.lesson.wordcount
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+ * @author yangqian
+ * @date 2021/4/29
+ */
+object WordCount {
+
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("WordCountScala").setMaster("spark://master:7077")
+    val sc = new SparkContext(conf)
+
+    val lines = sc.textFile("hdfs://master:9000/wordcount/spark.txt")
+    val words = lines.flatMap( line => line.split(" "))
+    val pairs = words.map(word => (word, 1))
+    val wordCounts = pairs.reduceByKey(_ + _)
+
+    wordCounts.foreach(wordCount => println(wordCount._1 + " appeared " + wordCount._2 + " times."))
+  }
+
+}
