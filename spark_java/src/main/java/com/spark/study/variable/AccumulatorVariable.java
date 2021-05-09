dd01526c6bb1778acaae96f119573d53b9587b2c
FrankQian <yangqian3@staff.weibo.com>
2021-05-09 18:01:46 +0800

    add : spark 共享变量

---
+++ spark_java/src/main/java/com/spark/study/variable/AccumulatorVariable.java
@@ -0,0 +1,36 @@
+package com.spark.study.variable;
+
+import org.apache.spark.Accumulator;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.VoidFunction;
+import org.apache.spark.util.AccumulatorV2;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author yangqian
+ * @date 2021/5/9
+ */
+public class AccumulatorVariable {
+
+    public static void main(String[] args) {
+        SparkConf conf = new SparkConf().setAppName("Accumulator").setMaster("local");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        final Accumulator<Integer> sum = sc.accumulator(0);
+        List<Integer> numberList = Arrays.asList(1, 2, 3, 4, 5);
+        JavaRDD<Integer> numbers = sc.parallelize(numberList);
+        numbers.foreach(new VoidFunction<Integer>() {
+            private static final long serialVersionUID = -6623107919175446580L;
+            @Override
+            public void call(Integer integer) throws Exception {
+                sum.add(integer);
+            }
+        });
+        System.out.println(sum);
+        sc.close();
+    }
+
+}
---
+++ spark_java/src/main/java/com/spark/study/variable/BroadcastVariable.java
@@ -0,0 +1,50 @@
+package com.spark.study.variable;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.VoidFunction;
+import org.apache.spark.broadcast.Broadcast;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author yangqian
+ * @date 2021/5/9
+ */
+public class BroadcastVariable {
+
+    public static void main(String[] args) {
+        SparkConf conf = new SparkConf().setAppName("BroadcastVariable").setMaster("local");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        final int factor = 3;
+        final Broadcast<Integer> factoBroadcast = sc.broadcast(factor);
+        List<Integer> numberList = Arrays.asList(1, 2, 3, 4, 5);
+        JavaRDD<Integer> numbers = sc.parallelize(numberList);
+
+        // 让集合中的每个数字都乘以外部定义的那个factor值
+        JavaRDD<Integer> multipleNumbers = numbers.map(new Function<Integer, Integer>() {
+            private static final long serialVersionUID = -1119341473428413030L;
+            @Override
+            public Integer call(Integer v1) throws Exception {
+                // 共享变脸
+                int factor = factoBroadcast.value();
+                return v1 * factor;
+            }
+        });
+
+        multipleNumbers.foreach(new VoidFunction<Integer>() {
+            private static final long serialVersionUID = 6332910179680904591L;
+            @Override
+            public void call(Integer integer) throws Exception {
+                System.out.println(integer);
+            }
+        });
+
+        sc.close();
+
+    }
+
+}
