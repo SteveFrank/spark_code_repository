7e3d1ae7c975a90d042569bffeb2ecf2881d69f6
FrankQian <yangqian3@staff.weibo.com>
2021-05-12 10:38:24 +0800

    add : spark sql

--- spark_java/src/main/java/com/spark/sql/study/dataset/DataFrameCreate.java
+++ spark_java/src/main/java/com/spark/sql/study/dataset/DataFrameCreate.java
@@ -5,9 +5,13 @@ import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SQLContext;
+import org.apache.spark.sql.SparkSession;
 
 
 /**
+ *
+ * Java 代码无法在本地跑，需要打包后才可以跑
+ *
  * 使用json文件创建dataframe
  * @author yangqian
  * @date 2021/5/11
@@ -17,14 +21,17 @@ public class DataFrameCreate {
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName("DataFrameCreate");
         JavaSparkContext sc = new JavaSparkContext(conf);
-        SQLContext sqlContext = new SQLContext(sc);
+        SQLContext sqlContext = SparkSession.builder()
+                .config(conf)
+                .getOrCreate()
+                .sqlContext();
 
+        // Java 代码无法在本地跑，需要打包后才可以跑
         Dataset<Row> dataset = sqlContext.read().json("hdfs://master:9000/spark_sql_file/students.json");
         dataset.show();
         // 打印DataFrame
         dataset.printSchema();
         // 查询某列的所有数据
-        dataset.select("name").show();
         dataset.select("name").show();
         // 查询某几列所有的数据，并对列进行计算
         dataset.select(dataset.col("name"), dataset.col("age").plus(1)).show();
--- spark_scala/src/main/scala/com/spark/sql/scala/lesson/DataSetCreate.scala
+++ spark_scala/src/main/scala/com/spark/sql/scala/lesson/DataSetCreate.scala
@@ -10,11 +10,11 @@ import org.apache.spark.{SparkConf, SparkContext}
 object DataSetCreate {
 
   def main(args: Array[String]): Unit = {
-    val conf = new SparkConf().setAppName("DataSetCreate")
+    val conf = new SparkConf().setAppName("DataSetCreate").setMaster("local[1]")
     val sc = new SparkContext(conf)
     // 注意重新学习SparkSession(使用方法略有不同)
     // SparkSession 可以替代 SqlContext
-    val sqlContext = new SQLContext(sc)
+    val sqlContext = SparkSession.builder().config(conf).getOrCreate()
     val dataset = sqlContext.read.json("hdfs://master:9000/spark_sql_file/students.json")
     dataset.show()
     dataset.show()
