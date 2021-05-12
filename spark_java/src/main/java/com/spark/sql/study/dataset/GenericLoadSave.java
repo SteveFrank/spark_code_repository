c0af38a2ad55b63b0c6f9c4138f97eb2047c7c51
FrankQian <yangqian3@staff.weibo.com>
2021-05-12 15:07:35 +0800

    add : spark DataSet load and save

---
+++ spark_java/src/main/java/com/spark/sql/study/dataset/GenericLoadSave.java
@@ -0,0 +1,29 @@
+package com.spark.sql.study.dataset;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SQLContext;
+
+/**
+ * @author yangqian
+ * @date 2021/5/12
+ */
+public class GenericLoadSave {
+
+    public static void main(String[] args) {
+        SparkConf conf = new SparkConf().setAppName("GenericLoadSave").setMaster("local");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        SQLContext sqlContext = new SQLContext(sc);
+
+        Dataset<Row> userDS = sqlContext.read().load("/Users/qian/WorkSpaces/own-workspace/2021/spark_code_repository/spark_java/src/main/resources/spark_sql/users.parquet");
+        userDS.printSchema();
+
+        userDS.show();
+
+        userDS.select("name", "favorite_color").show();
+        userDS.select("name", "favorite_color").write().save("/Users/qian/WorkSpaces/own-workspace/2021/spark_code_repository/spark_java/src/main/resources/spark_sql/namesAndFavColors.parquet");
+    }
+
+}
---
+++ spark_scala/src/main/scala/com/spark/sql/scala/lesson/dataset/GenericLoadSave.scala
@@ -0,0 +1,21 @@
+package com.spark.sql.scala.lesson.dataset
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+ * @author yangqian
+ * @date 2021/5/12
+ */
+object GenericLoadSave {
+
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("GenericLoadSave").setMaster("local")
+    val sc = new SparkContext(conf)
+    val sqlContext = SparkSession.builder().config(conf).getOrCreate()
+
+    val userDS = sqlContext.read.load("/Users/qian/WorkSpaces/own-workspace/2021/spark_code_repository/spark_java/src/main/resources/spark_sql/users.parquet")
+    userDS.show()
+  }
+
+}
