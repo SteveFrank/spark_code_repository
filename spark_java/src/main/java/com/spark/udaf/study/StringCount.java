0d2f38b81ffec80d477db448326c3d6b8fc618cc
FrankQian <yangqian3@staff.weibo.com>
2021-05-13 16:36:55 +0800

    add : 自定义udaf函数

---
+++ spark_java/src/main/java/com/spark/udaf/study/StringCount.java
@@ -0,0 +1,54 @@
+package com.spark.udaf.study;
+
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.expressions.MutableAggregationBuffer;
+import org.apache.spark.sql.expressions.UserDefinedAggregateFunction;
+import org.apache.spark.sql.types.DataType;
+import org.apache.spark.sql.types.StructType;
+
+/**
+ * @author yangqian
+ * @date 2021/5/13
+ */
+public class StringCount extends UserDefinedAggregateFunction {
+
+    @Override
+    public StructType inputSchema() {
+        return null;
+    }
+
+    @Override
+    public StructType bufferSchema() {
+        return null;
+    }
+
+    @Override
+    public DataType dataType() {
+        return null;
+    }
+
+    @Override
+    public boolean deterministic() {
+        return false;
+    }
+
+    @Override
+    public void initialize(MutableAggregationBuffer buffer) {
+
+    }
+
+    @Override
+    public void update(MutableAggregationBuffer buffer, Row input) {
+
+    }
+
+    @Override
+    public void merge(MutableAggregationBuffer buffer1, Row buffer2) {
+
+    }
+
+    @Override
+    public Object evaluate(Row buffer) {
+        return null;
+    }
+}
---
+++ spark_scala/src/main/scala/com/spark/udaf/scala/lesson/UDAF.scala
@@ -0,0 +1,38 @@
+package com.spark.udaf.scala.lesson
+
+import org.apache.spark.sql.types.{StringType, StructField, StructType}
+import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+ * @author yangqian
+ * @date 2021/5/13
+ */
+object UDAF {
+
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setMaster("local").setAppName("UDAF")
+    val sc = new SparkContext(conf)
+    val sqlContext = SparkSession.builder().config(conf).getOrCreate()
+
+    // 构造模拟数据
+    val names = Array("leo", "Marry", "Jack", "Tom", "Tom", "Tom", "Leo")
+    val namesRDD = sc.parallelize(names, 3)
+    val namesRowRDD = namesRDD.map( name => Row(name) )
+    val structType = StructType(Array(StructField("name", StringType, true)))
+    val nameDS = sqlContext.createDataFrame(namesRowRDD, structType)
+
+    // 注册表
+    nameDS.createOrReplaceTempView("names")
+
+    // 注册自定义函数
+    sqlContext.udf.register("strCount", new StringCount)
+
+    // 使用自定义函数
+    sqlContext.sql("select name, strCount(name) from names group by name")
+      .collect()
+      .foreach(println)
+
+  }
+
+}
